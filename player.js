40cdcbcea56a38252bbba5d9f22fdb4f09aa1132
Alfonda Dimas <alfondadims123@gmail.com>
2024-09-24 02:01:25 +0700

    first commit

--- player.js
+++ player.js
@@ -25,7 +25,6 @@ player.onpause = function () {
 player.ontimeupdate = function () {
     let ct = player.currentTime;
     current.innerHTML = timeFormat(ct);
-    //progress
     let duration = player.duration;
     prog = Math.floor((ct * 100) / duration);
     progress.style.setProperty("--progress", prog + "%");
@@ -41,3 +40,130 @@ function timeFormat(ct) {
 
     return minutes + ":" + seconds;
 }
+
+// quizz
+const questions = [
+    {
+        question: "dulu pernah dikasih survei katanya jadi stranger temen deket banget apa jadi pacar",
+        answers: [
+            { text: "pacar", correct: false },
+            { text: "temen deket", correct: false },
+            { text: "fiance", correct: false },
+            { text: "stranger", correct: true },
+        ]
+    },
+    {
+        question: "apa ada kesempatan ke-3 kalo kita bisa deket lagii?",
+        answers: [
+            { text: "jelas tidak", correct: false },
+            { text: "gatau yaya nya gimana", correct: false },
+            { text: "jelas iya", correct: false },
+            { text: "semuanya benar", correct: true },
+        ]
+    },
+    {
+        question: "setelah semua ini apakah gua benci sama lu?",
+        answers: [
+            { text: "jelas ngga karna gua benci peristiwanya aja bukan sama makhluknya", correct: true },
+            { text: "biasa aja", correct: false },
+            { text: "kenapa gue harus benci", correct: false },
+            { text: "semua nya salah", correct: false },
+        ]
+    },
+    {
+        question: "terus apa yang gua lakukan sekarang?",
+        answers: [
+            { text: "bengong", correct: false },
+            { text: "yaudah gitu aja", correct: true },
+            { text: "planga plongo", correct: false },
+            { text: "kesurupan", correct: false },
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButtons.firstChild) {
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+
+    nextButton.style.display = "block";
+}
+
+
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if (currentQuestionIndex < questions.length) {
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
+startQuiz();
